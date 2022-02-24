5c19c8b15c0377ed5c03560f1f3a1f2859cf5e86
user322123 <78637098+user322123@users.noreply.github.com>
2022-02-24 23:25:10 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+using System;
+/*************************/
+
+using lab_2;
+
+namespace lab_2t
+{
+    class Program
+    {
+
+        private static void lab_2()
+        {
+            ushort n1 = ((ushort)404);
+            RomanNumber conv1 = new RomanNumber(n1);
+            ushort n2 = ((ushort)272);
+            RomanNumber conv2 = new RomanNumber(n2);
+            Console.WriteLine("404 = " + conv1.ToString());
+            Console.WriteLine("272 = " + conv2.ToString());
+            Console.WriteLine("404 + 272 = 676 >>> " + conv1.ToString() + " + " + conv2.ToString() + " = " + RomanNumber.Add(conv1, conv2));
+            Console.WriteLine("404 - 272 = 676 >>> " + conv1.ToString() + " - " + conv2.ToString() + " = " + RomanNumber.Sub(conv1, conv2));
+            ushort n3 = ((ushort)16);
+            ushort n4 = ((ushort)8);
+            RomanNumber conv3 = new RomanNumber(n3);
+            RomanNumber conv4 = new RomanNumber(n4);
+            Console.WriteLine("16 * 8 = 128 >>> " + conv3.ToString() + " * " + conv4.ToString() + " = " + RomanNumber.Mul(conv3, conv4));
+            Console.WriteLine("16 / 8 = 2 >>> " + conv3.ToString() + " / " + conv4.ToString() + " = " + RomanNumber.Div(conv3, conv4));
+            short[] Values = new short[]
+            { 1000, 997, 500, 335, 105, 80, 44, 30, 20, 10, 6, 4, 2 };
+            RomanNumber[] conv5 = new RomanNumber[Values.Length];
+
+            Console.WriteLine("Initial array:");
+            for (int i = 0; i < Values.Length; ++i)
+            {
+                conv5[i] = new RomanNumber(((ushort)Values[i]));
+                Console.Write(Values[i] + "(" + conv5[i].ToString() + ")" + " ");
+            }
+            Console.WriteLine("\nFinal sorted array:");
+            Array.Sort(conv5);
+            foreach (object el in conv5)
+            {
+                Console.Write(el.ToString() + " ");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+           
+            lab_2();
+        }
+    }
+}
---
+++ RomanNumber.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text;
+using lab_2.ex;
+
+namespace lab_2
+{
+    public class RomanNumber : ICloneable, IComparable
+    {
+        private ushort Number;
+
+        private static short[] Val = new short[]
+        { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+        private static string[] Numerals = new string[]
+        { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        private short ToShort()
+        {
+            short res = ((short)Number);
+            return res;
+        }
+
+        public RomanNumber(ushort n)
+        {
+            short CheckException = ((short)n);
+            if (n <= 0)
+            {
+                throw new RomanNumberException("InvalidValueException: Ожидалось, что число > 0");
+            }
+            Number = n;
+        }
+
+        public static RomanNumber Add(RomanNumber? g1, RomanNumber? g2)
+        {
+            ushort result = 0;
+            short x1 = g1.ToShort();
+            short x2 = g2.ToShort();
+            short x3 = ((short)(x2 + x1));
+            result = ((ushort)x3);
+            RomanNumber res = new RomanNumber(result);
+            return res;
+        }
+
+        public static RomanNumber Sub(RomanNumber? g1, RomanNumber? g2)
+        {
+            ushort result = 0;
+            short x1 = g1.ToShort();
+            short x2 = g2.ToShort();
+            short x3 = ((short)(x1 - x2));
+            if (x3 <= 0)
+            {
+                throw new RomanNumberException("\nInvalidValueException: Ожидалось, что значение выражения (" + x3.ToString() + ") будет больше 0");
+            }
+            else
+            {
+                result = ((ushort)x3);
+                RomanNumber res = new RomanNumber(result);
+                return res;
+            }
+        }
+
+        public static RomanNumber Mul(RomanNumber? g1, RomanNumber? g2)
+        {
+            ushort result = 0;
+            short x1 = g1.ToShort();
+            short x2 = g2.ToShort();
+            short x3 = ((short)(x2 * x1));
+            result = ((ushort)x3);
+            RomanNumber res = new RomanNumber(result);
+            return res;
+        }
+
+        public static RomanNumber Div(RomanNumber? g1, RomanNumber? g2)
+        {
+            ushort result = 0;
+            short x1 = g1.ToShort();
+            short x2 = g2.ToShort();
+            short x3 = ((short)(x1 / x2));
+            if (x3 <= 0)
+            {
+                throw new RomanNumberException("InvalidValueException: Ожидалось, что результат выражение будет больше 0 (" + x3.ToString() + ")");
+            }
+            else
+            {
+                result = ((ushort)(x3));
+                RomanNumber res = new RomanNumber(result);
+                return res;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder NumberInRomanSystem = new StringBuilder();
+
+            int Range = 13;
+
+            short Temp = ((short)Number);
+
+            for (int i = 0; i < Range; ++i)
+            {
+                while (Temp >= Val[i])
+                {
+                    Temp -= Val[i];
+                    NumberInRomanSystem.Append(Numerals[i]);
+                }
+            }
+
+            return NumberInRomanSystem.ToString();
+        }
+
+        public object Clone()
+        {
+            return new RomanNumber(Number);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is RomanNumber rom) return ((short)Number).CompareTo(rom.ToShort());
+            else throw new RomanNumberException("InvaludValueException: Некорректное значение параметра");
+        }
+    }
+}
